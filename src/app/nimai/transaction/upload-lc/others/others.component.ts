faec25a597561910bb4baf456ad1f735490a4883
Geeta Gupta <geeta.gupta@cloverinfotech.com>
2020-07-07 14:17:17 +0530

    lcProForma file img upload added + time format changed

--- src/app/nimai/transaction/upload-lc/others/others.component.ts
+++ src/app/nimai/transaction/upload-lc/others/others.component.ts
@@ -11,7 +11,7 @@ export class OthersComponent implements OnInit {
   
   @Input() public LcDetail:FormGroup;
   fileToUpload: File = null;
-  
+  private imageSrc: string = '';
   
 
   constructor(public rds:DataServiceService) {
@@ -24,13 +24,25 @@ export class OthersComponent implements OnInit {
 
   }
 
-  handleFileInput1(files: FileList) {
-    this.fileToUpload = files.item(0);
-    console.log(this.fileToUpload);
-    const formData: FormData = new FormData();
-    formData.append('fileKey', this.fileToUpload, this.fileToUpload.name);
+  handleFileInput1(e) {
+    var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    var pattern = /image-*/;
+    var reader = new FileReader();
+    if (!file.type.match(pattern)) {
+      alert('invalid format');
+      return;
+    }
+    else{
+      reader.onload = this._handleReaderLoaded.bind(this);
+      reader.readAsDataURL(file);
+    }
     
-    this.LcDetail.get('lcProForma').setValue(formData);
   }
+  _handleReaderLoaded(e) {
+    let reader = e.target;
+    this.imageSrc = reader.result;
+    this.LcDetail.get('lcProForma').setValue(this.imageSrc);
 
+  }
 }
+
